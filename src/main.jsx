d0f3f6f207cb2b0f10c780f7dab7ca54f4f16c25
ruhul2003 <ruhul941020@gmail.com>
2026-04-03 01:30:45 +0600

    Added some router for starting

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Mobiles from './Components/Mobiles/Mobiles.jsx';
 import './index.css'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root.jsx';
+import Home from './Components/Home/Home.jsx';
+import Laptops from './Components/Laptops/Laptops.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true, Component:Home
+      },
+      {
+        path: "/mobiles",
+        Component: Mobiles
+      },
+      {
+        path: "/laptops",
+        Component: Laptops
+      }
+    ]
+  },
+  {
+    path: "/about",
+    element: <h1>This is about page</h1>,
+  }
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
+
